6c4d36d8c617b403f897d4b797bb8c1e450ac435
ginapult <gina.pultorak@gmail.com>
2019-09-17 18:21:54 -0700

    update scss

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -30,12 +30,12 @@ class Counter extends React.Component {
     let classes = ['count', this.state.polarity].join(' ');
     return (
       <section className="counter">
-        <a href="#" className="down clicker" onClick={this.handleDown}>
-          -
-        </a>
-        <span className={classes}>{this.state.count}</span>
         <a href="#" className="up clicker" onClick={this.handleUp}>
           +
+        </a>
+        <span className={classes}>{this.state.count}</span>
+        <a href="#" className="down clicker" onClick={this.handleDown}>
+          -
         </a>
       </section>
     );
